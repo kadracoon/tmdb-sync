51ff3aee933cd16e2dea3d6c9f594684ae20d296
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-07-04 19:09:59 +0500

    feat: add logging and error tracking for TMDB API failures

--- app/mongo.py
+++ app/mongo.py
@@ -6,3 +6,4 @@ client = AsyncIOMotorClient(settings.mongo_url)
 db = client[settings.mongo_db]
 movies_collection = db["movies"]
 frame_reports_collection = db["frame_reports_collection"]
+sync_errors_collection = db.sync_errors
